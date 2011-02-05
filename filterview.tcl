66dbf2c161286687159ccff12f4b153338ea8a62
Hans-Christoph Steiner <hans@eds.org>
2011-02-05 18:20:44 -0500

    got good behavior on the up/down filter line

--- filterview.tcl
+++ filterview.tcl
@@ -1,8 +1,8 @@
 
-set framex 30
-set framey 30
-set framewidth 300
-set frameheight 300
+set framex1 30
+set framey1 30
+set framex2 300
+set framey2 300
 
 set filterx 150
 set filtery 150
@@ -11,14 +11,23 @@ set filterheight 150
 
 proc startmovefilterbox {x y} {.c addtag movefilterbox closest $x $y}
 proc movefilterbox {x} {
-    set ::filterwidth [expr abs(($::framewidth/2) - $x)]
+    set ::filterwidth [expr abs(($::framex2/2) - $x)]
     puts stderr "filterwidth $::filterwidth"
-    .c coords movefilterbox [expr ($::filterwidth/2)-$::filterx] $::framey \
-         [expr ($::filterwidth/2)+$::filterx] [expr $::frameheight+$::framey]
+    .c coords movefilterbox [expr ($::filterwidth/2)-$::filterx] $::framey1 \
+         [expr ($::filterwidth/2)+$::filterx] [expr $::framey2+$::framey1]
 }
 
 proc startmovemey {x y} {.c addtag movemey closest $x $y}
-proc movemey {y} {.c coords movemey $::framex $y [expr $::framewidth+$::framex] $y}
+proc movemey {y} {
+    if {[expr $y < $::framey1]} {
+        set ::filterheight $::framey1
+    } elseif {[expr $y > $::framey2]} {
+        set ::filterheight $::framey2
+    } else {
+        set ::filterheight $y
+    }
+    .c coords movemey $::framex1 $::filterheight $::framex2 $::filterheight
+}
 
 proc stopmoveme {} {
     .c dtag movefilterbox
@@ -29,14 +38,14 @@ wm geometry . 400x400
 canvas .c
 pack .c -side left -expand 1 -fill both
 
-.c create rectangle $framex $framey [expr $framewidth+$framex] [expr $frameheight+$framey] \
+.c create rectangle $framex1 $framey1 $framex2 $framey2 \
     -tags filterframe -outline gray -fill "#eeeeff"
 
-.c create rectangle $filterx $framey [expr $filterwidth+$filterx] [expr $frameheight+$framey] \
+.c create rectangle $filterx $framey1 [expr $filterwidth+$filterx] $framey2 \
     -tags [list filterbox lines] \
     -outline red -activewidth 2
 
-.c create line $framex $filtery [expr $::framewidth+$::framex] $filtery \
+.c create line $framex1 $filterheight $::framex2 $filterheight \
     -tags [list horizontal lines] \
     -activewidth 2
 
