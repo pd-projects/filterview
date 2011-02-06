c85d4c1090e703e76d868d4969c8d42581483421
Hans-Christoph Steiner <hans@eds.org>
2011-02-06 18:23:39 -0500

    cleaned up interaction and code

--- filterview.tcl
+++ filterview.tcl
@@ -1,5 +1,5 @@
 
-console show
+catch {console show}
 # TODO filter out selections by tags using withtag
 # TODO there is two separate modes: 
 #     1) click and move filtercenter and gain (y).
@@ -22,22 +22,10 @@ set filtersideflag 1
 set previousx 0
 set previousy 0
 
-proc startmovefilter {mycanvas x y} {
-    puts stderr "startmovefilter $mycanvas $x $y"
-    set ::previousx $x
-    set ::previousy $y
-    if {$x < $::filtercenter} {
-        set ::filtersideflag 1
-    } else {
-        set ::filtersideflag 2
-    }
-    $mycanvas itemconfigure filterlines -width 2
-    $mycanvas bind filtergraph <Motion> "movefilter %W %x %y"
-}
+# colors
+set markercolor "#bbbbcc"
 
-proc movefilter {mycanvas x y} {
-    puts stderr "movefilter $mycanvas $x $y"
-# move bandwidth
+proc moveband {mycanvas x} {
     set dx [expr $x - $::previousx]
     set x1 [expr $::filterx1 + $dx]
     set x2 [expr $::filterx2 + $dx]
@@ -53,7 +41,9 @@ proc movefilter {mycanvas x y} {
     }
     $mycanvas coords filterband $::filterx1 $::framey1 $::filterx2 $::framey2
     set ::previousx $x
-# move gain
+}
+
+proc movegain {mycanvas y} {
     set gainy [expr $::filtergain + $y - $::previousy]
     if {[expr $gainy < $::framey1]} {
         set ::filtergain $::framey1
@@ -66,7 +56,39 @@ proc movefilter {mycanvas x y} {
     set ::previousy $y
 }
 
-proc changefilterwidth {mycanvas x y} {
+#------------------------------------------------------------------------------#
+# move the filter
+
+proc start_movefilter {mycanvas x y} {
+    puts stderr "start_movefilter $mycanvas $x $y"
+    set ::previousx $x
+    set ::previousy $y
+    if {$x < $::filtercenter} {
+        set ::filtersideflag 1
+    } else {
+        set ::filtersideflag 2
+    }
+    $mycanvas configure -cursor fleur
+    $mycanvas itemconfigure filterlines -width 2
+    $mycanvas bind filtergraph <Motion> "movefilter %W %x %y"
+}
+
+proc movefilter {mycanvas x y} {
+    moveband $mycanvas $x
+    movegain $mycanvas $y
+}
+
+#------------------------------------------------------------------------------#
+# change the filter
+
+proc start_changebandwidth {mycanvas x y} {
+    $mycanvas bind filtergraph <Motion> {changebandwidth %W %x %y}
+}
+
+proc stop_changebandwidth {mycanvas} {
+}
+
+proc changebandwidth {mycanvas x y} {
     if {$::filtersideflag == 1} {
         if {$x < $::framex1} {
             set ::filterx1 $::framey1
@@ -81,10 +103,25 @@ proc changefilterwidth {mycanvas x y} {
     }
 }
 
-proc stopmoveme {mycanvas} {
+proc enterband {mycanvas} {
+    $mycanvas configure -cursor center_ptr
+    $mycanvas itemconfigure filterband -width 2
+}
+
+proc leaveband {mycanvas} {
+    $mycanvas configure -cursor arrow
+    $mycanvas itemconfigure filterband -width 1
+}
+
+#------------------------------------------------------------------------------#
+
+proc stop_editing {mycanvas} {
     $mycanvas bind filtergraph <Motion> {}
     $mycanvas itemconfigure filterlines -width 1
+    $mycanvas configure -cursor arrow
 }
+
+#------------------------------------------------------------------------------#
 
 wm geometry . 400x400
 canvas .c
@@ -92,15 +129,28 @@ pack .c -side left -expand 1 -fill both
 
 # background
 .c create rectangle $framex1 $framey1 $framex2 $framey2 \
-    -outline "#bbbbcc" -fill "#eeeeff" \
+    -outline $markercolor -fill "#eeeeff" \
     -tags [list filtergraph]
+
 # bandwidth box
 .c create rectangle $filterx1 $framey1 $filterx2 $framey2 \
     -outline red -fill "#ebe8e8" \
     -tags [list filtergraph filterlines filterband]
+
+# midpoint
+set midpoint [expr (($::framey2 - $::framey1) / 2) + $::framey1]
+.c create line $::framex1 $midpoint $::framex2 $midpoint \
+    -fill $markercolor \
+    -tags [list filtergraph]
+
 # gain line
 .c create line $::framex1 $::filtergain $::framex2 $::filtergain \
     -tags [list filtergraph filterlines filtergain]
 
-.c bind filtergraph <ButtonPress-1> "startmovefilter %W %x %y"
-.c bind filtergraph <ButtonRelease-1> "stopmoveme %W"
+.c bind filtergraph <ButtonPress-1> {start_movefilter %W %x %y}
+.c bind filtergraph <ButtonRelease-1> {stop_editing %W}
+
+.c bind filterband <ButtonPress-1> {start_changebandwidth %W %x %y}
+.c bind filterband <ButtonRelease-1> {stop_editing %W}
+.c bind filterband <Enter> {enterband %W}
+.c bind filterband <Leave> {leaveband %W}
