c242885a1aa9531410f1dcb54cde2fce6f938010
Hans-Christoph Steiner <hans@eds.org>
2011-02-06 17:45:58 -0500

    got the basic mousedown movement working

--- filterview.tcl
+++ filterview.tcl
@@ -1,5 +1,9 @@
 
 console show
+# TODO filter out selections by tags using withtag
+# TODO there is two separate modes: 
+#     1) click and move filtercenter and gain (y).
+#     2) click on edges of band to adjust bandwidth/Q
 
 wm geometry . +500+40
 
@@ -12,7 +16,7 @@ set filterx1 100
 set filterx2 200
 set filtergain 150
 set filtercenter 150
-set filterbandwidth 50
+set filterwidth 50
 set filtersideflag 1
 
 set previousx 0
@@ -27,12 +31,42 @@ proc startmovefilter {mycanvas x y} {
     } else {
         set ::filtersideflag 2
     }
-    $mycanvas itemconfigure filtergraph -width 2
+    $mycanvas itemconfigure filterlines -width 2
     $mycanvas bind filtergraph <Motion> "movefilter %W %x %y"
 }
 
 proc movefilter {mycanvas x y} {
     puts stderr "movefilter $mycanvas $x $y"
+# move bandwidth
+    set dx [expr $x - $::previousx]
+    set x1 [expr $::filterx1 + $dx]
+    set x2 [expr $::filterx2 + $dx]
+    if {$x1 < $::framex1} {
+        set ::filterx1 $::framex1
+        set ::filterx2 [expr $::framex1 + $::filterwidth]
+    } elseif {$x2 > $::framex2} {
+        set ::filterx1 [expr $::framex2 - $::filterwidth]
+        set ::filterx2 $::framex2
+    } else {
+        set ::filterx1 $x1
+        set ::filterx2 $x2
+    }
+    $mycanvas coords filterband $::filterx1 $::framey1 $::filterx2 $::framey2
+    set ::previousx $x
+# move gain
+    set gainy [expr $::filtergain + $y - $::previousy]
+    if {[expr $gainy < $::framey1]} {
+        set ::filtergain $::framey1
+    } elseif {[expr $gainy > $::framey2]} {
+        set ::filtergain $::framey2
+    } else {
+        set ::filtergain $gainy
+    }
+    $mycanvas coords filtergain $::framex1 $::filtergain $::framex2 $::filtergain
+    set ::previousy $y
+}
+
+proc changefilterwidth {mycanvas x y} {
     if {$::filtersideflag == 1} {
         if {$x < $::framex1} {
             set ::filterx1 $::framey1
@@ -45,33 +79,11 @@ proc movefilter {mycanvas x y} {
         }
     } else {
     }
-    $mycanvas coords movefilter $::filterx1 $::framey1 $::filterx2 $::framey2
-    movegain $mycanvas $y
-}
-
-# TODO filter out selections by tags using withtag
-# TODO there is two separate modes: 
-#     1) click and move filtercenter and gain (y).
-#     2) click on edges of band to adjust bandwidth/Q
-proc movegain {mycanvas y} {
-    puts stderr "movegain $mycanvas $y"
-    set dy [expr $y - $::previousy]
-    set gainy [expr $::filtergain + $dy]
-    if {[expr $gainy < $::framey1]} {
-        set ::filtergain $::framey1
-    } elseif {[expr $gainy > $::framey2]} {
-        set ::filtergain $::framey2
-    } else {
-        set ::filtergain $gainy
-    }
-    $mycanvas coords filtergain $::framex1 $::filtergain $::framex2 $::filtergain
-    puts stderr "$mycanvas coords filtergain $::framex1 $::filtergain $::framex2 $::filtergain"
-    set ::previousy $y
 }
 
 proc stopmoveme {mycanvas} {
     $mycanvas bind filtergraph <Motion> {}
-    $mycanvas itemconfigure filtergraph -width 1
+    $mycanvas itemconfigure filterlines -width 1
 }
 
 wm geometry . 400x400
@@ -80,16 +92,15 @@ pack .c -side left -expand 1 -fill both
 
 # background
 .c create rectangle $framex1 $framey1 $framex2 $framey2 \
-    -outline "#eeeeee" -fill "#eeeeff" \
+    -outline "#bbbbcc" -fill "#eeeeff" \
     -tags [list filtergraph]
 # bandwidth box
 .c create rectangle $filterx1 $framey1 $filterx2 $framey2 \
-    -tags [list filtergraph filterbandwidth] \
-    -outline red -activewidth 2
+    -outline red -fill "#ebe8e8" \
+    -tags [list filtergraph filterlines filterband]
 # gain line
 .c create line $::framex1 $::filtergain $::framex2 $::filtergain \
-    -tags [list filtergraph filtergain]
+    -tags [list filtergraph filterlines filtergain]
 
 .c bind filtergraph <ButtonPress-1> "startmovefilter %W %x %y"
 .c bind filtergraph <ButtonRelease-1> "stopmoveme %W"
-
